1cff028be835cb09b45076ae1695a6f24ebbd770
Jake Dumont <jakedumont@Jake-Dumonts-MacBook-Pro.local>
2017-07-22 05:51:31 -0700

    combining server files

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var http = require('http');
 var express = require('express');
 var twilio = require('twilio');
+const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
 
+var db;
 var app = express();
 
-http.createServer(app).listen(1337, function() {
-	console.log('Express server listening on port 1337');
-});
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds034677.mlab.com:34677/ubiquitous-waddle', function(err, database) {
+	if (err) return console.log(err)
+	db = database;
+	app.listen(1337, function() {
+		console.log('Express server listening on port 1337');
+	})
+})
+
+// this is for getting the contents of the html into the database
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.post('/sms', function(req, res) {
 	var twilio = require('twilio');
@@ -16,10 +26,39 @@ app.post('/sms', function(req, res) {
 	res.end(twiml.toString());
 })
 
+// this reads from HTML POST call
+app.post('/shelters', function(req, res) {
+	db.collection('shelters').save(req.body, function(err, result) {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
+})
+
+// this reads from HTML POST call
+app.post('/missingPersons', function(req, res) {
+	db.collection('missingPersons').save(req.body, function(err, result) {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
+})
+
+// this reads from HTML POST call
+app.post('/hazards', function(req, res) {
+	db.collection('hazards').save(req.body, function(err, result) {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
+})
+
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
 	console.log(__dirname)
 	res.sendFile(__dirname + '/disaster-relief.html');
 })
-
